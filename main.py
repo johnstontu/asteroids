7d3cbe6b87b420250b7561db3c581c391232fa2d
johnstontu <tuckerjohnston13@gmail.com>
2025-01-15 21:57:24 -0500

    Add shooting

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import sys
 import pygame
 import random
 from constants import *
 from circleshape import *
 from player import *
 from asteroid import *
+from shot import *
 
 def main():
     pygame.init()
@@ -14,10 +16,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -30,6 +34,11 @@ def main():
 
         for obj in updatable:
             obj.update(dt)
+        
+        for obj in asteroids:
+            if obj.collision_detect(player):
+                print("Game over!")
+                sys.exit()
 
         for obj in drawable:
             obj.draw(screen)
--- player.py
+++ player.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import *
 from circleshape import *
+from shot import *
 
 class Player(CircleShape):
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
+        
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +31,7 @@ class Player(CircleShape):
          self.position += forward * PLAYER_SPEED * dt
     
     def update(self, dt):
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -40,5 +45,18 @@ class Player(CircleShape):
         
         if keys[pygame.K_w]:
             self.move(dt)
+        
+
+        if keys[pygame.K_SPACE]:
+             if self.shoot_timer <= 0:
+                self.shoot()
+        
+        
+    def shoot(self):
+         self.shoot_timer = PLAYER_SHOOT_COOLDOWN
+         shot = Shot(self.position.x, self.position.y)
+         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+         
+    
     
 
